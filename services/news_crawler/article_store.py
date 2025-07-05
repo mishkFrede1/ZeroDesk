11ba5d07cadcff7675d7d2765760719218bea158
YamanakaKazuo <damirmukanov8@gmail.com>
2025-07-05 20:36:22 +0500

    added docker compose and Dockerfile files for Docker

---
+++ services/news_crawler/article_store.py
@@ -0,0 +1,18 @@
+from sqlalchemy.exc import IntegrityError
+from database import SessionLocal, ParsedArticle
+from datetime import datetime
+
+def is_article_exists(url: str) -> bool:
+    with SessionLocal() as session:
+        return session.query(ParsedArticle).filter(ParsedArticle.url == url).first() is not None
+
+def save_article(url: str, title: str, published_at: datetime):
+    with SessionLocal() as session:
+        article = ParsedArticle(url=url, title=title, published_at=published_at)
+        session.add(article)
+
+        try:
+            session.commit()
+        except IntegrityError:
+            session.rollback() # Article already exists
+
