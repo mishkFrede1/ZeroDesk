7237cd197a8d90a4628f5d004954e4cfc35b96c4
YamanakaKazuo <damirmukanov8@gmail.com>
2025-07-30 18:42:45 +0500

    [FIX] Fix bug with unnable connection in docker

--- services/web_ui/web_ui/settings.py
+++ services/web_ui/web_ui/settings.py
@@ -155,7 +155,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": os.environ["REDIS"],  # "redis://localhost:6379/1"
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
